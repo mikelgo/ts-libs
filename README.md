65413ee35e4587c54b0f68e75822c62ea0dab603
Michael Berger <m.gonser@icloud.com>
2022-06-27 15:13:44 +0200

    lib/ts-guards: move existing ts-guards lib

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## Libraries
 * ts-memoize: a memoization function and decorator: [see](./packages/ts-memoize/README.md)
+* ts-guards: a utility library providing guard functions [see](./packages/ts-guards/README.md)
---
+++ packages/ts-guards/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'ts-guards',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/ts-guards',
+};
